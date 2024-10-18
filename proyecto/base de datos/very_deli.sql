59b81547a3a326491e67543ca4477758e067ad27
RingoNero <110137808+joaquinmej@users.noreply.github.com>
2024-10-18 19:34:15 -0300

    base de datos sin relaciones

---
+++ proyecto/base de datos/very_deli.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-10-2024 a las 00:33:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `very deli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `calificacion`
+--
+
+CREATE TABLE `calificacion` (
+  `idCalificacion` int(11) NOT NULL,
+  `idCalifica` int(11) NOT NULL,
+  `puntaje` int(11) NOT NULL,
+  `comentario` text NOT NULL,
+  `idCalificado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensaje`
+--
+
+CREATE TABLE `mensaje` (
+  `idUsuario` int(11) NOT NULL,
+  `idPublicacion` int(11) NOT NULL,
+  `comentario` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `postulacion`
+--
+
+CREATE TABLE `postulacion` (
+  `idPostulacion` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `monto` float NOT NULL,
+  `idPublicacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `publicacion`
+--
+
+CREATE TABLE `publicacion` (
+  `idPublicacion` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `volumen` float NOT NULL,
+  `peso` float NOT NULL,
+  `origen` varchar(40) NOT NULL,
+  `destino` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `apellido` varchar(20) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `responsable` tinyint(1) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `codPostal` int(11) NOT NULL,
+  `contraseña` varchar(100) NOT NULL,
+  `imagen` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `patente` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  ADD PRIMARY KEY (`idCalificacion`),
+  ADD KEY `idCalifica` (`idCalifica`,`idCalificado`);
+
+--
+-- Indices de la tabla `mensaje`
+--
+ALTER TABLE `mensaje`
+  ADD KEY `idUsuario` (`idUsuario`,`idPublicacion`);
+
+--
+-- Indices de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  ADD PRIMARY KEY (`idPostulacion`),
+  ADD KEY `idUsuario` (`idUsuario`,`idPublicacion`);
+
+--
+-- Indices de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  ADD PRIMARY KEY (`idPublicacion`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `dni` (`dni`),
+  ADD UNIQUE KEY `contraseña` (`contraseña`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `imagen` (`imagen`) USING HASH,
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idUsuario_2` (`idUsuario`);
+
+--
+-- Indices de la tabla `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`patente`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `calificacion`
+--
+ALTER TABLE `calificacion`
+  MODIFY `idCalificacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `postulacion`
+--
+ALTER TABLE `postulacion`
+  MODIFY `idPostulacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `publicacion`
+--
+ALTER TABLE `publicacion`
+  MODIFY `idPublicacion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
